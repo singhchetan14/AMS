0db13e10a8aac67ced72901298b40e31b4a71872
Upashana7 <upretiupashana@gmail.com>
2026-04-13 11:23:57 +0545

    Add files via upload

---
+++ assets/js/admin_script.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+  document.getElementById("sidebar").classList.toggle("hide");
+  document.getElementById("main").classList.toggle("full");
+  document.getElementById("topbar").classList.toggle("full");
+}
